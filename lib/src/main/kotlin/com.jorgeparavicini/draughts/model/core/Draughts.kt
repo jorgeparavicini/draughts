e066b43d75911d5636b534723d4f830f63449f0d
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-05-24 15:22:29 +0200

    Add Move executed handler

--- lib/src/main/kotlin/com.jorgeparavicini/draughts/model/core/Draughts.kt
+++ lib/src/main/kotlin/com.jorgeparavicini/draughts/model/core/Draughts.kt
@@ -7,6 +7,7 @@ import com.jorgeparavicini.draughts.model.exceptions.IllegalMoveException
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger { }
+typealias MoveExecutedHandler = (Move) -> Unit
 
 class Draughts(
     private val blackController: Controller,
@@ -34,6 +35,8 @@ class Draughts(
     val currentController: Controller
         get() = if (currentPlayer == Player.BLACK) blackController else whiteController
 
+    private var onMoveExecutedHandler: MoveExecutedHandler? = null
+
     init {
         blackController.player = Player.BLACK
         whiteController.player = Player.WHITE
@@ -52,6 +55,10 @@ class Draughts(
     fun startGame() {
         if (hasStarted) throw IllegalStateException("Game already started")
         hasStarted = true
+    }
+
+    fun setOnMoveExecutedHandler(handler: MoveExecutedHandler) {
+        onMoveExecutedHandler = handler
     }
 
     suspend fun nextTurn() {
@@ -74,9 +81,10 @@ class Draughts(
             if (isGameOver) break
             val move = controller.getMove()
             try {
-                if (!field.executeMove(move, currentPlayer)) break
+                val didEat = field.executeMove(move, currentPlayer)
+                onMoveExecutedHandler?.invoke(move)
+                if (!didEat) break
             } catch (e: IllegalMoveException) {
-
                 logger.trace("Move was not allowed: $move")
                 controller.illegalMove(move, e.message)
             }
